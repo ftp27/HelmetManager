9058382d285cce299a625d5655e05c68101c7042
Aleksey Cherepanov <ftp27host@gmail.com>
2014-06-10 21:29:09 +0400

    Add jquery-ui. Dialogs for create directory and delete

--- src/main/resources/site/js/file.js
+++ src/main/resources/site/js/file.js
@@ -1,4 +1,10 @@
 $(".nav-content").ready(function() {
+    $(this).keypress(function (e) {
+        if (e.which == 46) {
+            $(".file-delete").click();
+        }
+    });
+
 	$('.file-address').keypress(function (e) {
         if (e.which == 13) {
             setList($(this).closest(".file-part").find(".file-list"),$(this).val());
@@ -8,20 +14,130 @@ $(".nav-content").ready(function() {
 	$(".file-list").each( function() {
         setList($(this),'/sdcard');
     });
+
+    // Dialog windows
+
+    $("#file-dialog-error").dialog({
+            autoOpen: false,
+            resizable: false,
+            dialogClass: "no-close",
+            height:250,
+            width:450,
+            modal: true,
+            buttons: {
+                Ok: function() {
+                    $( this ).dialog( "close" );
+                }
+            }
+    });
+
+    $("#file-dialog-delete").dialog({
+        autoOpen: false,
+        resizable: false,
+        dialogClass: "no-close",
+        height:250,
+        width:450,
+        modal: true,
+        buttons: {
+            "Delete": function() {
+
+                $.post(
+                    "/file/",
+                    {
+                        action: "delete",
+                        file: $(".file-selected").attr("src"),
+                    },
+                    function(data) {
+                        if (data.Status == "error") {
+                            $("#file-dialog-error>p").text(data.Code);
+                            $("#file-dialog-error").dialog( "open" );
+                        }
+                        updateLists();
+                    },
+                    "json"
+                );
+                $( this ).dialog( "close" );
+            },
+            Cancel: function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    });
+
+    $("#file-dialog-newdir").dialog({
+        autoOpen: false,
+        resizable: false,
+        dialogClass: "no-close",
+        height:250,
+        width:450,
+        modal: true,
+        buttons: {
+            "Create": function() {
+
+                $.post(
+                    "/file/",
+                    {
+                        action: "newdir",
+                        file: $("#file-newdir-address").val()+'/'+$("#file-newdir-name").val(),
+                    },
+                    function(data) {
+                        if (data.Status == "error") {
+                            $("#file-dialog-error>p").text(data.Code);
+                            $("#file-dialog-error").dialog( "open" );
+                        }
+                        updateLists();
+                    },
+                    "json"
+                );
+                $( this ).dialog( "close" );
+            },
+            Cancel: function() {
+                $( this ).dialog( "close" );
+            }
+        }
+    });
+
+
+
+    //Button actions
+
+    $(".file-delete").click(function() {
+        fileAddress = $(".file-selected").attr("src");
+        if (fileAddress != "") {
+            $("#file-dialog-delete>p>span").text(fileAddress);
+            $("#file-dialog-delete").dialog( "open" );
+        }
+    });
+
+    $(".file-newdir").click(function() {
+        fileAddress = $(".file-selected").parent("ul").attr("src");
+        if (fileAddress != "") {
+            $("#file-newdir-address").val(fileAddress);
+            $("#file-dialog-newdir").dialog( "open" );
+        }
+    });
+
 });
+
+function updateLists() {
+    $(".file-list").each( function() {
+        address = $(this).children("ul").attr("src");
+        setList($(this),address);
+    });
+}
 
 function setList(dom, address) {
         $.getJSON("/file/"+address).done( function( data ) {
             side = dom.parents(".file-part").attr('id');
 
             if (data["fileType"] == "directory") {
-                list = "<ul>";
+                list = "<ul src='"+address+"'>";
                 $.each( data["files"], function( key, val ) {
                     //console.log(val["fileName"]);
                     if (val["fileName"] == "..") {
                         dom.parent().find(".file-upbutton").attr("src",val["fileAddress"]);
                     }
-                    elem = "<li id='"+side+"-"+key+"' src='"+val["fileAddress"]+"'>";
+                    elem = "<li id='"+side+"-"+key+"' src='"+val["fileAddress"]+"' class='file-dont-selected'>";
                     if (val["fileType"] == "directory") {
                         elem += "<img src='/res/img/dir.png'>";
                     } else {
@@ -61,14 +177,8 @@ function updateEvents() {
     );
 
     $('.file-list>ul>li').click(function () {
-        fileRows = $(".file-list>ul>li");
-        fileRows.css("background-color","white");
-        fileRows.css("color","black");
-        fileRows.children(".file-name").css("color","black");
-
-        $(this).css("background-color","#809ECC");
-        $(this).children(".file-name").css("color","white");
-        $(this).css("color","white");
+        $(".file-selected").switchClass("file-selected","file-dont-selected",50);
+        $(this).switchClass("file-dont-selected","file-selected",50)
     });
 
     $('.file-name').click(function () {
